a20236703b739a3ea7bb36d015f1b92c0cb33cad
umitora7240 <kaito-s-17d@stu.kbc.ac.jp>
2019-01-30 13:04:37 +0900

    PlayerのScoreに応じてPlayerPrafsのソート　ボタンの作成

--- PanCrash/Assets/SceneResult/SceneResultManager.cs
+++ PanCrash/Assets/SceneResult/SceneResultManager.cs
@@ -2,23 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-
-public class SceneResultManager : MonoBehaviour {
+public class SceneResultManager : MonoBehaviour
+{
 
     private Text ScoreText;
+    private int score;
+    public static readonly string[] KEY = { "0", "1", "2", "3", "4" };
+    int length;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
+        ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        //score = SceneMain.GetScore();
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ScoreText.text = score.ToString();
+    }
+
+  public  void ChangeScene()
+    {
+        Debug.Log("Change");
+
+        for(int i = 0; i<length; i++)
+        {
+            if(score>  PlayerPrefs.GetInt(KEY[i], 0))
+            {
+                for(int j=length-1; j>i; j--)
+                {
+                    PlayerPrefs.SetInt(KEY[j], PlayerPrefs.GetInt(KEY[j - 1], 0));
+                }
+                PlayerPrefs.SetInt(KEY[i], score);
+                break;
+            }
+        }
+
+        //SceneManager.LoadScene("SceneRanking");
+
+    }
 
 
 }
---
+++ PanCrash/Assets/SceneResult/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour {
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+
+        //SceneResultManager.key = "aa";
+        //SceneResultManager.keyconst=
+        string a = SceneResultManager.KEY[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+	}
+}
